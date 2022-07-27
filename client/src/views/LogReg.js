3ff67e5aad73a05ca835dc7d2b4efba20eca8f39
Sunny Garcia <sunnyjr1994@gmail.com>
2022-07-26 17:39:08 -0700

    added Login and Register js, view, and App.js

---
+++ client/src/views/LogReg.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../components/Login';
+import Register from '../components/Register';
+
+const LogReg = (props) => {
+    return(
+        <div>
+            <Login />
+            <Register />
+        </div>
+    )
+}
+
+export default LogReg;
