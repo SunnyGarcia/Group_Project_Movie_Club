2e23fcda7167d8ca6e65a6f6d6ec09bb4aa9b2c1
Sunny Garcia <94879028+SunnyGarcia@users.noreply.github.com>
2022-07-26 17:48:29 -0700

    Revert "added Login and Register.js, view, and App.js"

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,25 @@
-import React from 'react';
-import LogReg from "./views/LogReg";
-// import BudgetForm from "./components/BudgetForm";
-// import OneBudget from "./components/OneBudget";
-// import UpdateBudget from "./components/UpdateBudget";
-// import LogReg from "./views/LogReg";
-// import Profile from "./components/Profile";
-// import ExpenseForm from "./components/ExpenseForm";
-// import UpdateExpense from "./components/UpdateExpense";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-    return (
-        <div className="App">
-            <BrowserRouter>
-            <Routes>
-                <Route element={<LogReg />} path="/" />
-                {/* <Route element={<BudgetForm/>} path="/new/budgets" />
-                <Route element={<OneBudget/>} path="/budgets/:id" />
-                <Route element={<UpdateBudget/>} path="/edit/budgets/:id" />
-                <Route element={<LogReg />} path="/" />
-                <Route element={<Profile />} path="/user/profile/:username" />
-                <Route element={<ExpenseForm />} path="/new/expenses" />
-                <Route element={<UpdateExpense />} path="edit/expenses/:id" /> */}
-
-            </Routes>
-            </BrowserRouter>
-        </div>
-    )
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
-
